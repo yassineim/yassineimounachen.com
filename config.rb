6ebf831c54db1ce80dc884ca20eeb5a9aafc0156
Yassine Imounachen <yassine.imounachen@gmail.com>
2019-02-24 07:39:16 +0000

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,11 +1,2 @@
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
 set :relative_links, true
 activate :i18n
-
-configure :build do
-  activate :minify_css
-  activate :minify_html
-end
